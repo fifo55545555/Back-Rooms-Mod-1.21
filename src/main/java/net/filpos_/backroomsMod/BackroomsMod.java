62c5d3653a7dfb8a1b8dee843025bee8160db7b2
Filpos_ <fifokrapek@gmail.com>
2025-07-02 18:59:16 +0200

    MOD_ID -problem

--- src/main/java/net/filpos_/backroomsMod/BackroomsMod.java
+++ src/main/java/net/filpos_/backroomsMod/BackroomsMod.java
@@ -1,6 +1,8 @@
 package net.filpos_.backroomsMod;
 
+import net.filpos_.backroomsMod.block.ModBlocks;
 import net.filpos_.backroomsMod.item.ModItems;
+
 import net.minecraft.world.item.CreativeModeTabs;
 import org.slf4j.Logger;
 
@@ -41,7 +43,10 @@ public class BackroomsMod {
         // Note that this is necessary if and only if we want *this* class (ExampleMod) to respond directly to events.
         // Do not add this line if there are no @SubscribeEvent-annotated functions in this class, like onServerStarting() below.
         NeoForge.EVENT_BUS.register(this);
+
         ModItems.register(modEventBus);
+        ModBlocks.register(modEventBus);
+
         // Register the item to a creative tab
         modEventBus.addListener(this::addCreative);
 
@@ -58,10 +63,11 @@ public class BackroomsMod {
 
         if(event.getTabKey() == CreativeModeTabs.FOOD_AND_DRINKS) {
             event.accept(ModItems.ALMOND_WATER);
+            event.accept(ModItems.DIRTY_ALMOND_WATER);
         }
 
-        if(event.getTabKey() == CreativeModeTabs.INGREDIENTS){
-            event.accept(ModItems.DIRTY_ALMOND_WATER);
+        if(event.getTabKey() == CreativeModeTabs.BUILDING_BLOCKS) {
+            event.accept(ModBlocks.YELLOW_WALLPAPER);
         }
     }
 
---
+++ src/main/java/net/filpos_/backroomsMod/block/ModBlocks.java
@@ -0,0 +1,47 @@
+package net.filpos_.backroomsMod.block;
+
+import net.filpos_.backroomsMod.BackroomsMod;
+import net.filpos_.backroomsMod.item.ModItems;
+import net.minecraft.client.resources.sounds.Sound;
+import net.minecraft.world.item.AxeItem;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.SoundType;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredBlock;
+import net.neoforged.neoforge.registries.DeferredRegister;
+
+import java.util.function.Supplier;
+
+public class ModBlocks {
+
+    public static final DeferredRegister.Blocks BLOCKS = DeferredRegister.createBlocks(BackroomsMod.MOD_ID);
+
+
+    //new blocks here
+    public static final DeferredBlock<Block> YELLOW_WALLPAPER = registerBlock("yellow_wallpaper",
+            () -> new Block(BlockBehaviour.Properties.of()
+                    .strength(2f)
+                    .requiresCorrectToolForDrops()
+                    .sound(SoundType.BAMBOO_WOOD)
+            ));
+
+
+    // methods
+    private static <T extends Block> DeferredBlock<T> registerBlock(String name, Supplier<T> block){
+        DeferredBlock<T> toReturn = BLOCKS.register(name, block);
+        registerBlockItem(name, toReturn);
+        return toReturn;
+    }
+
+    private static <T extends Block> void registerBlockItem(String name, DeferredBlock<T> block){
+        ModItems.ITEMS.register(name, () -> new BlockItem(block.get(), new Item.Properties()));
+    }
+
+    public static void register(IEventBus eventBus) {
+        BLOCKS.register(eventBus);
+    }
+
+}
